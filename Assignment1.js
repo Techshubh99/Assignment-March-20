570078cd6137c011a582c90f67c018757aa3130e
Shubham Rao <96663535+Techshubh99@users.noreply.github.com>
2022-03-20 18:57:52 +0530

    Add files via upload

---
+++ Assignment1.js
@@ -0,0 +1,30 @@
+// Create four functions to print the result add, sub, div, mul
+function sum(num1,num2){
+    let result = num1 + num2;
+    return result;
+}
+function sub(num1,num2){
+    let result = num1 - num2;
+    return result;
+}
+function mul(num1,num2){
+    let result = num1 * num2;
+    return result;
+}
+function div(num1,num2){
+    let result = num1 / num2;
+    return result;
+
+}
+
+s = sum(2,3);
+console.log(s);
+
+t = sub(5,3);
+console.log(t)
+
+m = mul(6,3);
+console.log(m)
+
+d = div(6,2);
+console.log(d);
---
+++ assignment2.js
@@ -0,0 +1,8 @@
+function WholeSquare(a,b){
+    let result = (a*a)+(b*b)+2*(a*b);
+    return result;
+
+}
+
+ws = WholeSquare(2,3)
+console.log(ws);
